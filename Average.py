3952c7dbec0775d31981be791d69d4853aab813b
Ayanacyan <amitpacl@gmail.com>
2025-12-28 20:59:36 -0600

    Averaging

---
+++ Average.py
@@ -0,0 +1,11 @@
+tree1=98
+tree2=45
+tree3=78
+tree4=67
+tree5=91
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of the variables is:",sum)
+
+average=sum/5
+print("The average of the variables is:", average)
